ffa2d633af571cf163e9fa54d501a39154fdc4b1
bSek316303 <bs316303@student.polsl.pl>
2024-11-20 21:27:24 +0100

    1

--- minImaxElement.cpp
+++ minImaxElement.cpp
@@ -3,9 +3,16 @@
 int main ()
 {
     constexpr int ROZMIAR = 20;
+    int min = 0;
+    int max = 0;
     int tab[ROZMIAR];
     for (int i = 0; i < ROZMIAR; i++)
     {
-        
+        tab[i] = rand() % 20;
+    }
+    cout << tab;
+    for (int i = 0; i < ROZMIAR; i++)
+    {
+        if (i = 0)
     }
 }
